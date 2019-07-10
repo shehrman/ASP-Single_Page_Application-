9df8479bcd5b0568f6ce68b9607901dc4963d2c6
Ibrahim, Shehrman <shehrman.ibrahim@cgi.com>
2019-07-10 14:03:27 +0200

    Add movisControl and generic DBContext

---
+++ SPAProjectMovies/Data/SPAProjectMoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SPAProjectMovies.Models
+{
+    public class SPAProjectMoviesContext : DbContext
+    {
+        public SPAProjectMoviesContext (DbContextOptions<SPAProjectMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SPAProjectMovies.Models.Movie> Movie { get; set; }
+    }
+}
--- SPAProjectMovies/Startup.cs
+++ SPAProjectMovies/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using SPAProjectMovies.Models;
 
 namespace SPAProjectMovies
 {
@@ -26,6 +28,9 @@ namespace SPAProjectMovies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SPAProjectMoviesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SPAProjectMoviesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +47,12 @@ namespace SPAProjectMovies
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
